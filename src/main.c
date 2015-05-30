f2375e7d813f572b0fda8fce65e05cfc19b35c86
letskillbrad <letskillbrad@gmail.com>
2015-05-30 11:39:07 -0400

    Watch Works
    Watch works, next up: open/closed status

--- src/main.c
+++ src/main.c
@@ -2,8 +2,9 @@
 
 static Window *s_main_window;
 TextLayer *s_time_layer;
-//TextLayer *s_status_layer;
+TextLayer *s_status_layer;
 static GFont s_time_font;
+static GFont s_status_font;
 
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
@@ -29,6 +30,10 @@ static void update_time() {
   text_layer_set_text(s_time_layer, buffer);
 }
 
+/*static void update_status() {
+}
+*/
+
 static void main_window_load(Window *window) {
   //Create GBitmap, set to BitmapLayer
   s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
@@ -37,16 +42,27 @@ static void main_window_load(Window *window) {
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
   
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DIN_32));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DIN_BOLD_32));
+  s_status_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DIN_24));
   
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(5, 102, 139, 40));
+  s_time_layer = text_layer_create(GRect(5, 102, 134, 38));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  
+  // Create status TextLayer
+  s_status_layer = text_layer_create(GRect(5, 140, 134, 24));
+  text_layer_set_background_color(s_status_layer, GColorClear);
+  text_layer_set_text_color(s_status_layer, GColorWhite);
+  text_layer_set_font(s_status_layer, s_status_font);
+  text_layer_set_text_alignment(s_status_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_status_layer, "OPEN");
+  
 
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_status_layer));
   
   update_time();
 }
@@ -60,7 +76,13 @@ static void main_window_unload(Window *window) {
   
   //unload GFont
   fonts_unload_custom_font(s_time_font);
+  fonts_unload_custom_font(s_status_font);
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_status_layer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
 static void init() {
@@ -77,7 +99,7 @@ static void init() {
   window_stack_push(s_main_window, true);
   
   // Register with TickTimerService
-  //tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   
 }
 
